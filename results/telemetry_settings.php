2ffbc562b0b7bafe5b6e8ec37ff8002c07187660
Christopher Jorge Paes de Barros Silva <christopher.jorge@kpershow.com>
2021-03-08 13:39:17 -0400

    Tirando itens não usados

--- results/telemetry_settings.php
+++ results/telemetry_settings.php
@@ -1,9 +1,9 @@
 <?php
 
 // Type of db: "mysql", "sqlite" or "postgresql"
-$db_type = 'mysql';
+$db_type = 'postgresql';
 // Password to login to stats.php. Change this!!!
-$stats_password = 'PASSWORD';
+$stats_password = 'Mineirinho@21';
 // If set to true, test IDs will be obfuscated to prevent users from guessing URLs of other tests
 $enable_id_obfuscation = false;
 // If set to true, IP addresses will be redacted from IP and ISP info fields, as well as the log
@@ -13,14 +13,14 @@ $redact_ip_addresses = false;
 $Sqlite_db_file = '../../speedtest_telemetry.sql';
 
 // Mysql settings
-$MySql_username = 'USERNAME';
-$MySql_password = 'PASSWORD';
-$MySql_hostname = 'DB_HOSTNAME';
-$MySql_databasename = 'DB_NAME';
-$MySql_port = '3306';
+//$MySql_username = 'USERNAME';
+//$MySql_password = 'PASSWORD';
+//$MySql_hostname = 'DB_HOSTNAME';
+//$MySql_databasename = 'DB_NAME';
+//$MySql_port = '3306';
 
 // Postgresql settings
-$PostgreSql_username = 'USERNAME';
-$PostgreSql_password = 'PASSWORD';
-$PostgreSql_hostname = 'DB_HOSTNAME';
-$PostgreSql_databasename = 'DB_NAME';
+$PostgreSql_username = 'ruralfoneveloc';
+$PostgreSql_password = 'Mineirinho@21';
+$PostgreSql_hostname = 'ruralfoneveloc.postgresql.dbaas.com.br';
+$PostgreSql_databasename = 'ruralfoneveloc';
